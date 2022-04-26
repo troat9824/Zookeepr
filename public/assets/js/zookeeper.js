dacfeda9fc847193351d83d59c8d2a793da649f7
troat9824 <troat9824@gmail.com>
2022-04-26 06:18:24 -0500

    zookeeper

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,3 +1,4 @@
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
+router.use(require('./zookeeperRoutes'));
 router.use(animalRoutes);
 
 module.exports = router;
